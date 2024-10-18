4003329caadfc9b3b7603bb2559cd646504d8822
Zuharul Habib <zuharul.habib.ft22@mail.umy.ac.id>
2024-10-18 20:05:33 +0700

    menambahkan text ini adalah halaman login

--- app/src/main/java/com/example/activitypam3/MainActivity.kt
+++ app/src/main/java/com/example/activitypam3/MainActivity.kt
@@ -56,6 +56,8 @@ fun BasicComposable(modifier: Modifier = Modifier) {
             color = Color.Red,
             fontWeight = FontWeight.Bold
         )
+        Text(text = "Ini Adalah Halaman Login",
+        )
 
 
     }
